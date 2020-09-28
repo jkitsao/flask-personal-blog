4d19d4f1713532c40d11f99aff24694c6fff8ac2
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-09-29 01:31:40 +0300

    add blog test file

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -5,12 +5,12 @@ from app.models import Blog,User
 
 class BlogMOdelTest(unittest.TestCase):
    def setUp(self):
-       self.user_hannah = User(username = 'hannah',password = 'hannah')
-       self.new_blog = Blog(content='hannah')
+       self.user_kitsao = User(username = 'kitsao',password = 'kitsao')
+       self.new_blog = Blog(content='kitsao')
 
 
    def test_check_instance_variable(self):
-       self.assertEquals(self.new_blog.content,'hannah')
+       self.assertEquals(self.new_blog.content,'kitsao')
 
 
 
--- tests/test_user.py
+++ tests/test_user.py
@@ -5,7 +5,7 @@ from app import db
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User(password = 'hannah')
+        self.new_user = User(password = 'kitsao')
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
@@ -15,4 +15,4 @@ class UserModelTest(unittest.TestCase):
                 self.new_user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('hannah'))    
+        self.assertTrue(self.new_user.verify_password('kitsao'))    
