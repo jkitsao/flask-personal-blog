17f709d74c409351668803dbbca102f1399390e3
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-09-29 01:40:12 +0300

    migrations to heroku db

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='jkitsao@gmail.com'
-export MAIL_PASSWORD='scarletshevannah'
+export MAIL_PASSWORD='neutrinokitsao'
 export SECRET_KEY='#$%SER##@SDER$#%'
 export DATABASE_URL='postgres://osjdxommkpajya:331568f21e2091dbffde86f61ff2c8d8598cf8d44482174f7d4d2fcc29c5e143@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d4ji8t73b1mtp5'
 
